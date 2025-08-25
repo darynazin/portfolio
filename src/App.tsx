1a98d9f8c8fd5b876db2579b5a8770e1d638bd6d
darynazin <danayu@ukr.net>
2025-08-25 13:09:56 +0200

    refine general styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import './App.css'
 import Experience from './components/Exporience/Experience'
-import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Hero from './components/Hero/Hero'
 import Stack from './components/Stack/Stack'
 import Projects from './components/Projects/Projects'
-import SpaceBackground from './components/SpaceBackground/SpaceBackground'
 import Contacts from './components/Contacts/Contacts'
 
 function App() {
 
   return (
     <div className="layout-container">
-      <SpaceBackground />
       <main className="main-container">
         <Header />
         <Hero />
@@ -20,7 +17,6 @@ function App() {
         <Stack />
         <Projects />
         <Contacts />
-        <Footer />
       </main>
     </div>
   )
